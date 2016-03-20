6c9653e0d93acc9635e143efbf368b217b4943c2
psaniuk <pavel.saniuk@gmail.com>
2016-03-20 21:08:36 +0100

    ImageView

---
+++ WinToolkit.Samples/WinToolkit.Controls/ImageView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WinToolkit.Controls
+{
+    public class ImageView: Control
+    {
+        private Image _image;
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(Uri), typeof(ImageView), 
+            new PropertyMetadata(default(Uri), new PropertyChangedCallback(OnSourcePropertyChanged)));
+
+        public ImageView()
+        {
+            DefaultStyleKey = typeof(ImageView);
+        }
+
+        public Uri Source
+        {
+            get
+            {
+                return (Uri)GetValue(SourceProperty);
+            }
+
+            set
+            {
+                SetValue(SourceProperty, value);
+            }
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _image = (Image) GetTemplateChild("ImageControl");
+        }
+
+        private async void OnSourceChanged()
+        {
+            if (Source == null)
+                return;
+
+
+            try
+            {
+                byte[] buffer = await _httpClient.GetByteArrayAsync(Source);
+                if (buffer != null && buffer.Any())
+                {
+
+                }
+            }
+            catch(HttpRequestException)
+            {
+
+            }
+        }
+
+        private static void OnSourcePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (args.NewValue != null)
+                ((ImageView)sender).OnSourceChanged();
+        }
+    }
+}
---
+++ WinToolkit.Samples/WinToolkit.Samples/Page2.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace WinToolkit.Samples
+{
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
