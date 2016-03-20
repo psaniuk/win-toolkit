a240a3cbcb5df66f07005c3c1653d9779fb4b87e
psaniuk <pavel.saniuk@gmail.com>
2016-03-20 21:26:42 +0100

    introduced ImageBackgroundProcessor and Pixel

---
+++ WinToolkit.Samples/WinToolkit.Controls/Imaging/ImageBackgroundProcessor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace WinToolkit.Controls.Imaging
+{
+    internal sealed class ImageBackgroundProcessor
+    {
+        private readonly bool[][] _isVisited;
+        private readonly int _pixelWidth;
+        private readonly int _pixelHeight;
+
+        public ImageBackgroundProcessor(int pixelWidth, int pixelHeight)
+        {
+            _isVisited = new bool[pixelHeight][];
+            _pixelWidth = pixelWidth;
+            _pixelHeight = pixelWidth;
+        }
+
+        public void Process(byte[] pixelsData)
+        {
+            var queue = new Queue<Pixel>();
+            var firstPixel = new Pixel(pixelsData, 0, _pixelWidth, _pixelHeight);
+            queue.Enqueue(firstPixel);
+            MarkAsVisited(firstPixel);
+
+
+            while (queue.Count > 0)
+            {
+                Pixel pixel = queue.Dequeue();
+                pixel.ChangeColor(150);
+
+                Pixel[] adjacents = pixel.GetAdjacents();
+                foreach (Pixel adj in adjacents)
+                {
+                    if (adj.IsWhite && !IsVisited(adj))
+                    {
+                        MarkAsVisited(adj);
+                        queue.Enqueue(adj);
+                    }
+                }
+            }
+        }
+
+        private void MarkAsVisited(Pixel pixel)
+        {
+            int index = pixel.Index;
+            int row = index / _pixelWidth;
+            int col = index % _pixelWidth;
+
+            if (_isVisited[row] == null)
+                _isVisited[row] = new bool[_pixelWidth];
+
+            _isVisited[row][col] = true;
+
+        }
+
+        private bool IsVisited(int row, int col)
+        {
+            if (_isVisited[row] == null)
+                return false;
+
+            return _isVisited[row][col];
+        }
+
+        private bool IsVisited(Pixel pixel) => IsVisited(pixel.Row, pixel.Col);
+    }
+}
---
+++ WinToolkit.Samples/WinToolkit.Controls/Imaging/Pixel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+namespace WinToolkit.Controls.Imaging
+{
+    internal sealed class Pixel
+    {
+        private readonly int _pixelsWidth;
+        private readonly int _pixelsHeight;
+        private readonly byte[] _pixels;
+
+        public Pixel(byte[] pixels, int index, int pixelsWidth, int pixelsHeight)
+        {
+            _pixels = pixels;
+            Index = index;
+            _pixelsWidth = pixelsWidth;
+            _pixelsHeight = pixelsHeight;
+        }
+
+        public int Index { get; private set; }
+
+        public bool IsWhite
+        {
+            get
+            {
+
+                byte color = 240;
+                int pixelsDataIndex = Index * 4;
+                for (int i = pixelsDataIndex; i < pixelsDataIndex + 4; i++)
+                    if (_pixels[i] < color)
+                        return false;
+
+                return true;
+            }
+        }
+
+        public void ChangeColor(byte newColor)
+        {
+            int pixelsDataIndex = Index * 4;
+            for (int i = pixelsDataIndex; i < pixelsDataIndex + 4; i++)
+                _pixels[i] = newColor;
+        }
+
+        public Pixel[] GetAdjacents()
+        {
+            var result = new List<Pixel>();
+
+            int row = Row;
+            int col = Col;
+
+            if (row > 0)
+                AddPixel(GetMatrixIndex(row - 1, col), result);
+
+            if (row < _pixelsHeight - 1)
+                AddPixel(GetMatrixIndex(row + 1, col), result);
+
+
+            if (col > 0)
+                AddPixel(GetMatrixIndex(row, col - 1), result);
+
+
+            if (col < _pixelsWidth - 1)
+                AddPixel(GetMatrixIndex(row, col + 1), result);
+
+
+            if (row > 0 && col > 0)
+                AddPixel(GetMatrixIndex(row - 1, col - 1), result);
+
+
+            if (row < _pixelsHeight - 1 && col > 0)
+                AddPixel(GetMatrixIndex(row + 1, col - 1), result);
+
+
+            if (row > 0 && col < _pixelsWidth - 1)
+                AddPixel(GetMatrixIndex(row - 1, col + 1), result);
+
+
+            if (row < _pixelsHeight - 1 && col < _pixelsWidth - 1)
+                AddPixel(GetMatrixIndex(row + 1, col + 1), result);
+
+
+            return result.ToArray();
+        }
+
+        public int Row => Index / _pixelsWidth;
+        public int Col => Index % _pixelsWidth;
+        private int GetMatrixIndex(int row, int col) => row * _pixelsWidth + col;
+
+        private void AddPixel(int index, List<Pixel> adjacents)
+        {
+            var pixel = new Pixel(_pixels, index, _pixelsWidth, _pixelsHeight);
+            adjacents.Add(pixel);
+        }
+    }
+}
